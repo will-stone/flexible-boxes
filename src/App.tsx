55179a01fcb70a7cad05d6dbf4c12028ff03b64c
Will Stone <654103+will-stone@users.noreply.github.com>
2022-01-05 19:35:57 +0000

    chore: add support for Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p className="text-xl">
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a href="https://reactjs.org" rel="noopener noreferrer" target="_blank">
+        Learn React
+      </a>
     </div>
   )
 }
